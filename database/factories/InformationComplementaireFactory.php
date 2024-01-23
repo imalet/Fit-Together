8eb15c455f1a661cd6652e068d9e48e478aed95a
iMALET Benji <imaletbenji58@gmail.com>
2024-01-23 18:41:36 +0000

    Tous passe Bien ! meme les resources

---
+++ database/factories/InformationComplementaireFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\InformationComplementaire>
+ */
+class InformationComplementaireFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "user_id" => 1,
+            "bio" => "Ma bio",
+            "qualification" => "Ma Qualification",
+            "experience" => "Mon Experience",
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Categorie;
+use App\Models\InformationComplementaire;
 use App\Models\Post;
 use App\Models\Role;
 use App\Models\User;
@@ -28,6 +29,9 @@ class DatabaseSeeder extends Seeder
             ->create();
 
         Post::factory(7)
+            ->create();
+        
+        InformationComplementaire::factory(13)
             ->create();
 
         // \App\Models\User::factory()->create([
